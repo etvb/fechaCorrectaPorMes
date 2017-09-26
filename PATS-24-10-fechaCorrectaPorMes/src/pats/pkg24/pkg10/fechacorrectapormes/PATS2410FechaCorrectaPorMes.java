017b8f553afd844fe015d6c124bf1eb4ea34872e
Edgardo Torres Vanden Bouch <edgardo.torres@hotmail.com>
2017-09-25 19:02:35 -0500

    archivos base

---
+++ PATS-24-10-fechaCorrectaPorMes/src/pats/pkg24/pkg10/fechacorrectapormes/PATS2410FechaCorrectaPorMes.java
@@ -0,0 +1,20 @@
+/*
+ * Pedir el día, mes y año de una fecha e indicar si la fecha es correcta.
+ * Con meses de 28, 30, 31 días. Sin años bisiestos. 
+ */
+package pats.pkg24.pkg10.fechacorrectapormes;
+
+/**
+ *
+ * @author edgardo
+ */
+public class PATS2410FechaCorrectaPorMes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
